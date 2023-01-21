88f0bfd048760a428f97a08b491677e31bc28290
jiyoonjung1106 <120460487+jiyoonjung1106@users.noreply.github.com>
2023-01-21 23:44:05 +0900

    day02

--- src/day01/MainClass02.java
+++ src/day01/MainClass02.java
@@ -1,5 +1,22 @@
 package day01;
 
-public class MainClass02 {
+/*
+ * 	주석 - 컴퓨터가 인식하지 않는 영역
+ * 
+ * 	여러줄 주석입니다.
+ */
+
+//	(한줄주석)자바 코드 시작 class입니다.
+// 자바 코드는 class 예약어 { } 안에 정의
+
+public class MainClass02 {	
+	/** 
+	 * JavaDoc 주석
+	 * 메인메소드 - 자바 처음 실행되는 메소드
+	 */
+	public static void main(String[] args) {
+//		Console 출력 명령어
+		System.out.println("안녕하세요");
+	}
 
 }
